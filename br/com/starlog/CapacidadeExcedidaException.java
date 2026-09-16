5947c31fe310c55d8aa4e0583defce442efb7a2d
matheusmaziero2005-tech <matheus.maziero2005@gmail.com>
2026-09-16 10:29:53 -0300

    Add files via upload

---
+++ br/com/starlog/CapacidadeExcedidaException.java
@@ -0,0 +1,8 @@
+package br.com.starlog.exception;
+
+public class CapacidadeExcedidaException extends Exception{
+    public CapacidadeExcedidaException (String mensagem){
+        super(mensagem);
+    }
+    
+}
